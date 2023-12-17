de13922c73bc2712282637ded1701a33f3993512
Demon <emmanuelonyekachi04122000@gmail.com>
2023-12-17 20:34:33 +0000

    Updated header file

--- monty.h
+++ monty.h
@@ -1,5 +1,5 @@
-#ifndef MONTY_H_
-#define MONTY_H_
+#ifndef MONTY_H
+#define MONTY_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -17,10 +17,11 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -31,6 +32,34 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+extern instruction_t **operation_list;
+instruction_t **operation_list;
+void create_operation_list(void);
+void print_stack_content(stack_t **stack, unsigned int line_number);
+void free_stack_memory(stack_t *rear);
+void free_all_resources(stack_t **stack, char *opcode, char *arg);
+void pop_stack_top(stack_t **stack, unsigned int line_number);
+int tokenize_input_line(char *buf, char **opcode, char **arg);
+void add_instruction(instruction_t **d, char *oc, void (*f)(stack_t**, unsigned int));
+void free_instructions(void);
+void print_file_error(char *file);
+void print_usage_error(void);
+void print_malloc_error(void);
+void print_top_stack_element(stack_t **stack, unsigned int line_number);
+void swap_top_two_elements(stack_t **stack, unsigned int line_number);
+void add_top_two_elements(stack_t **stack, unsigned int line_number);
+void subtract_top_two_elements(stack_t **stack, unsigned int line_number);
+void multiply_top_two_elements(stack_t **stack, unsigned int line_number);
+void divide_top_two_elements(stack_t **stack, unsigned int line_number);
+void modulo_top_two_elements(stack_t **stack, unsigned int line_number);
+void print_char(stack_t **stack, unsigned int line_number);
+void print_string(stack_t **stack, unsigned int line_number);
+void rotate_right(stack_t **stack, unsigned int line_number);
+void rotate_left(stack_t **stack, unsigned int line_number);
+void check_arguments(stack_t **stack, unsigned int line_number, char *op, char *arg);
+
+#endif
